664528c671ed7cdb876b8e531417142ded45a73b
hlnicks <hlnicks@icloud.com>
2022-06-28 21:21:42 -0400

    added inquirer

---
+++ app.js
@@ -0,0 +1,26 @@
+const inquirer = require ('inquirer');
+
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portolio complete! Check out index.html to see output.');
+// });
