51b29098544fc23b961af1d8332326f2b744d2e5
hlnicks <hlnicks@icloud.com>
2022-07-01 22:20:12 -0400

    modified HTML template to handle more data

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require ('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,25 +131,12 @@ const promptProject = portfolioData => {
     });
 };
 
-
-
-
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+        console.log('Page created! Checkout index.html in this directory to see it!');
+    });
 });
-
-
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portolio complete! Check out index.html to see output.');
-// });
